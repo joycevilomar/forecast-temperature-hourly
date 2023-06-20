8ce45857e1a43ce25eb63ca741f74c80fe329dd0
Joyce Vilomar <vilomar.joyce@gmail.com>
2023-06-20 14:16:49 -0400

    added new changes

---
+++ source/temp-hourly.r
@@ -0,0 +1,8 @@
+# Nombre: Joyce N. Vilomar
+# Diplomado Econometría
+
+setwd("C:/Personal/Proyectos/forecast-temperature-hourly/source/data")
+tempData <- read.csv("MLTempDataset.csv")
+View(tempData) 
+
+names(tempData)
